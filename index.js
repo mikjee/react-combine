e9abcd8c3ef43c416fa483abbe1ea0aaa42eb53f
MikJee <soumik.chat@hotmail.com>
2021-07-07 14:24:57 +0530

    Fixed core exports

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ import applyEnhancers from './src/redux/applyEnhancers'
 
 import Combine from './src/components/Combine'
 
-import { canParent } from './src/framework/core'
+import {
+	generateLayerId,
+	createLayer,
+	deleteLayer,
+	mountLayer,
+	setParent,
+	canParent,
+	reorderChildren,
+	setInputs,
+	getProjection,
+	renameLayer
+} from './src/framework/core'
+
 import { getUserAPI } from './src/framework/API'
 
 export default Combine
@@ -11,6 +23,17 @@ export default Combine
 export {
 	applyEnhancers,
 	reducerMap,
+
+	generateLayerId,
+	createLayer,
+	deleteLayer,
+	mountLayer,
+	setParent,
 	canParent,
+	reorderChildren,
+	setInputs,
+	getProjection,
+	renameLayer,
+
 	getUserAPI
 }
